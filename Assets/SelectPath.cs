5a60d93cb889316ead6d32595b7c607b5d01c285
bloodbloodblood <orangeredresden@gmail.com>
2013-10-12 23:17:30 -0700

    the things
    yep

---
+++ Assets/SelectPath.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectPath : MonoBehaviour 
+{
+	//public int i, j;
+	
+	private bool firstCube, secondCube;
+	private int firstI, firstJ, secondI, secondJ;
+	
+	
+	// Use this for initialization
+	void Start () 
+	{
+		//print ("even starting??? in here??? ");
+		//firstCube = false;
+		//secondCube = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	//void Clicked()
+	//{
+	//	renderer.material.color = Color.green;
+	//}
+	
+ 	void OnMouseDown()
+	{
+		if(!firstCube && (renderer.material.color != Color.black))
+		{
+			print ("first: " + firstCube);
+			renderer.material.color = Color.green;
+			//firstI = i;
+			//firstJ = j;
+			this.firstCube = true;
+		}
+		else if(renderer.material.color != Color.black && firstCube && !secondCube)
+		{
+			print ("2nd");			
+			renderer.material.color = Color.green;
+			//secondI = i;
+			//secondJ = j;
+			this.secondCube = true;
+		}
+ 	}   	
+}
---
+++ Assets/SpawnObjects.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnObjects : MonoBehaviour {
+	
+	public GameObject spawnObject;
+
+	
+	public float spawnAreaWidth;
+	public float spawnAreaHeight;
+	public int xTiles;
+	public int yTiles;
+	public int randomThreshold; 
+	
+
+	
+	public GameObject[,] tileMap; //make 3d array later for tileheights?
+	
+	// Use this for initialization
+	void Start () 
+	{
+
+		int random;
+		tileMap = new GameObject[xTiles,yTiles];
+		
+
+		for(int i = 0; i < xTiles; i++)
+		{
+			for(int j = 0; j < yTiles; j++)
+			{
+				Vector3 spawnPosition = transform.position;
+				spawnPosition.x += i * (spawnAreaWidth/xTiles);
+				spawnPosition.y += j * (spawnAreaHeight/yTiles);
+				GameObject newObject = Instantiate(spawnObject,spawnPosition,spawnObject.transform.rotation) as GameObject;
+
+				random = Random.Range(0,100);
+				if(random >= randomThreshold)
+				{
+					newObject.renderer.material.color = Color.black;		
+				}
+				else
+				{
+					newObject.renderer.material.color = Color.white;	
+				}
+				newObject.transform.parent = transform; //makes objects children of manager
+				//s.i = i; //save indexes of squares
+				//s.j = j;
+				//newObject.GetComponent<SelectPath>().i = i;
+				//newObject.GetComponent<SelectPath>().j = j;
+				
+
+				//print ("bef array i,j:"+i.ToString()+ ":" +j.ToString());
+
+				tileMap[i,j] = newObject;;
+				//print ("after array i,j:"+i.ToString()+ ":" +j.ToString());
+			}
+		}	
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnDrawGizmos() 
+	{		
+		for(int i = 0; i < xTiles; i++)
+		{
+			for(int j = 0; j < yTiles; j++)
+			{
+				Vector3 spawnPosition = transform.position;
+				spawnPosition.x += i * (spawnAreaWidth/xTiles);
+				spawnPosition.y += j * (spawnAreaHeight/yTiles);
+				Gizmos.DrawLine(spawnPosition + Vector3.left,spawnPosition + Vector3.right);
+				Gizmos.DrawLine(spawnPosition + Vector3.up,spawnPosition + Vector3.down);
+				Gizmos.DrawLine(spawnPosition + Vector3.forward,spawnPosition + Vector3.back);
+			}
+		}
+	}	
+}
